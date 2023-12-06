86734498b5628d7e6490cc90bffa93fa1c25e33b
JuanBoho <juanbohorquez.ar@gmail.com>
2023-12-05 23:50:05 -0300

    Entrega03 - Airflow

---
+++ Entrega03/dags/sql/create_tables.sql
@@ -0,0 +1,57 @@
+CREATE TABLE IF NOT EXISTS locations_stg (
+    location_id INTEGER,
+    name VARCHAR(100),
+    country VARCHAR(100),
+    city VARCHAR(100),
+    sensor_type VARCHAR(100),
+    longitude DOUBLE PRECISION,
+    latitude DOUBLE PRECISION,
+    last_updated TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS locations (
+    location_id INTEGER distkey,
+    name VARCHAR(100),
+    country VARCHAR(100),
+    city VARCHAR(100),
+    sensor_type VARCHAR(100),
+    longitude DOUBLE PRECISION,
+    latitude DOUBLE PRECISION,
+    last_updated TIMESTAMP
+)
+SORTKEY (location_id);
+
+CREATE TABLE IF NOT EXISTS aq_measuraments_stg (
+    location_id INTEGER,
+    date DATE,
+    temperature DOUBLE PRECISION,
+    pressure DOUBLE PRECISION,
+    humidity DOUBLE PRECISION,
+    voc DOUBLE PRECISION,
+    um025 DOUBLE PRECISION,
+    um003 DOUBLE PRECISION,
+    um005 DOUBLE PRECISION,
+    um050 DOUBLE PRECISION,
+    um100 DOUBLE PRECISION,
+    pm1 DOUBLE PRECISION,
+    pm10 DOUBLE PRECISION,
+    um010 DOUBLE PRECISION,
+    pm25 DOUBLE PRECISION
+);
+CREATE TABLE IF NOT EXISTS aq_measuraments (
+    location_id INTEGER distkey,
+    date DATE,
+    temperature DOUBLE PRECISION,
+    pressure DOUBLE PRECISION,
+    humidity DOUBLE PRECISION,
+    voc DOUBLE PRECISION,
+    um025 DOUBLE PRECISION,
+    um003 DOUBLE PRECISION,
+    um005 DOUBLE PRECISION,
+    um050 DOUBLE PRECISION,
+    um100 DOUBLE PRECISION,
+    pm1 DOUBLE PRECISION,
+    pm10 DOUBLE PRECISION,
+    um010 DOUBLE PRECISION,
+    pm25 DOUBLE PRECISION
+)
+SORTKEY (date);
